d02e0f668a120f1aa68115c2315a656cb6957ba5
pedro49max <pedrotobalina49@gmail.com>
2026-02-20 11:32:45 +0100

    fn compute_bounds_infix_operation
    function to compute the bound of the result of an infix operation

--- constraint_generation/src/compute_bounds.rs
+++ constraint_generation/src/compute_bounds.rs
@@ -8,6 +8,7 @@ use program_structure::ast::{Expression, Meta, Statement};
 use program_structure::error_definition::ReportCollection;
 use program_structure::program_archive::ProgramArchive;
 use std::collections::HashMap;
+use std::ops::{Add, AddAssign, Div, Sub};
 use crate::FlagsExecution;
 use program_structure::ast::ExpressionInfixOpcode;
 use compiler::hir::very_concrete_program::Bounds;
@@ -127,14 +128,15 @@ fn treat_substitution(stmt: &Statement, context: &HashMap<String, Bounds>, envir
 
 fn compute_bounds_expression(
     expr: &Expression, context: &HashMap<String, Bounds>, environment: &EE, prime: &String)
-->Option<Bounds>{
+->Bounds{
     use Expression::*;
 
+    let no_bounds = Bounds{min: BigInt::from(0), max: BigInt::from(6)};
     match expr{
-            InfixOp{  .. }=>todo!(),
+            InfixOp{  lhe, rhe, infix_op,.. }=>compute_bounds_infix_operation(lhe, rhe, *infix_op, context, environment, prime),
             PrefixOp {  .. }=>todo!(),
             InlineSwitchOp { .. }=>todo!(),
-            ParallelOp { .. }=>None,
+            ParallelOp { .. }=>no_bounds,
             Variable { .. }=>todo!(),
             Number{  .. }=>todo!(),
             Call{ .. }=>None,
@@ -148,9 +150,95 @@ fn compute_bounds_expression(
 }
 
 
-fn compute_bounds_infix_operation(expr_l: &Expression, expr_r: &Expression, operator: ExpressionInfixOpcode){
+fn compute_bounds_infix_operation(expr_l: &Expression, expr_r: &Expression, operator: ExpressionInfixOpcode, context: &HashMap<String, Bounds>, environment: &EE, prime: &BigInt)->Bounds{
     // check if the operands have bounds and compute the bounds of the 
     // result using them
+    let bl = compute_bounds_expression(expr_l, context, environment, prime);
+    let br = compute_bounds_expression(expr_r, context, environment, prime);
 
+
+    match operator {
+        program_structure::ast::ExpressionInfixOpcode::Mul =>{
+         Bounds{
+            min: bl.min * (br.min),
+            max: bl.max * (br.max)
+        }},
+        program_structure::ast::ExpressionInfixOpcode::Div => Bounds{
+            min: BigInt::from(0),
+            max: prime.clone()
+        },
+        program_structure::ast::ExpressionInfixOpcode::Add => Bounds{
+            min: bl.min + br.min,
+            max: bl.max + br.max
+        },
+        program_structure::ast::ExpressionInfixOpcode::Sub => Bounds{
+            min: bl.min - br.max,
+            max: bl.max - br.min
+        },
+        program_structure::ast::ExpressionInfixOpcode::Pow => Bounds{
+            min: bl.min.min(BigInt::from(1)),//Either 0 or 1
+            max: prime.clone()
+        },
+        program_structure::ast::ExpressionInfixOpcode::IntDiv => Bounds{
+            min: BigInt::from(0),//if br > bl, the result is 0
+            max: bl.max //In integer division, the result is not going to be biger than the dividend
+        },
+        program_structure::ast::ExpressionInfixOpcode::Mod => Bounds{
+            min: BigInt::from(0),//if the left operand is a multiple of the right operand, the result is 0
+            max: br.max//In Mod the resuult is not going to be bigger than the right operand
+        },
+        program_structure::ast::ExpressionInfixOpcode::ShiftL => Bounds{
+            min:  bl.min * 2i32.pow(br.min.to_u32().unwrap()),
+            max:  bl.max * 2i32.pow(br.max.to_u32().unwrap())
+        },
+        program_structure::ast::ExpressionInfixOpcode::ShiftR => Bounds{
+            min: BigInt::from(0),
+            max: bl.max //In right shift, the result is not going to be bigger than the left operand
+        },
+        program_structure::ast::ExpressionInfixOpcode::LesserEq => Bounds{
+            min:BigInt::from(0),// 0 or  1
+            max:BigInt::from(1),// 0 or  1
+        },
+        program_structure::ast::ExpressionInfixOpcode::GreaterEq => Bounds{
+            min: BigInt::from(0),// 0 or  1
+            max: BigInt::from(1),// 0 or  1
+        },
+        program_structure::ast::ExpressionInfixOpcode::Lesser => Bounds{
+            min: BigInt::from(0),// 0 or  1
+            max: BigInt::from(1),// 0 or  1
+        },
+        program_structure::ast::ExpressionInfixOpcode::Greater => Bounds{
+            min: BigInt::from(0),// 0 or  1
+            max: BigInt::from(1),// 0 or  1
+        },
+        program_structure::ast::ExpressionInfixOpcode::Eq => Bounds{
+            min: BigInt::from(0),// 0 or  1
+            max: BigInt::from(1),// 0 or  1
+        },
+        program_structure::ast::ExpressionInfixOpcode::NotEq => Bounds{
+            min: BigInt::from(0),// 0 or  1
+            max: BigInt::from(1),// 0 or  1
+        },
+        program_structure::ast::ExpressionInfixOpcode::BoolOr => Bounds{
+            min: BigInt::from(0),// 0 or  1
+            max: BigInt::from(1),// 0 or  1
+        },
+        program_structure::ast::ExpressionInfixOpcode::BoolAnd => Bounds{
+            min: BigInt::from(0),// 0 or  1
+            max: BigInt::from(1),// 0 or  1
+        },
+        program_structure::ast::ExpressionInfixOpcode::BitOr => Bounds{
+            min: bl.min.max(br.min),
+            max: bl.max + br.max
+        },
+        program_structure::ast::ExpressionInfixOpcode::BitAnd => Bounds{
+            min: BigInt::from(0),
+            max: bl.max.min(br.max)
+        },
+        program_structure::ast::ExpressionInfixOpcode::BitXor => Bounds{
+           min: BigInt::from(0),
+            max: bl.max + br.max
+        },
+    }
 
 }
